39df501cb1b25be4527f536d5b6ac67b841979c7
ishaagrawal67 <isha.agrawal_cs20@gla.ac.in>
2022-06-23 16:22:23 +0530

    by escape character

---
+++ src/byEScapeCharacter.java
@@ -0,0 +1,6 @@
+public class byEScapeCharacter {
+    public static void main(String[] args) {
+        String str="Dear Isha, \n\tYou are such a nice \n\t Girl";
+        System.out.println(str);
+    }
+}
