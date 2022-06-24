80f41c9bfc44302a1dba409aecdf37966b3a169a
ishaagrawal67 <isha.agrawal_cs20@gla.ac.in>
2022-06-24 11:49:50 +0530

    convert string lower case

---
+++ src/convertStringIntoLOwerCase.java
@@ -0,0 +1,11 @@
+import java.util.Locale;
+import java.util.Scanner;
+public class convertStringIntoLOwerCase {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        String  str= sc.next();
+        String str2=str.toLowerCase();
+        System.out.println(str2);
+
+    }
+}
