0828306d01c6689e2611ae1f622fdc564cccce5a
ishaagrawal67 <isha.agrawal_cs20@gla.ac.in>
2022-06-24 11:52:45 +0530

    find double and tripple spaces

---
+++ src/finddoubleandtripplespaces.java
@@ -0,0 +1,7 @@
+public class finddoubleandtripplespaces {
+    public static void main(String[] args) {
+        String str = "to find  double and   tripple spaces";
+        System.out.println(str.indexOf("  "));
+        System.out.println(str.indexOf("   "));
+    }
+}
