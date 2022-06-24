0fd9ebee8b6be30aea10cc64f3bfdba5c3b91a96
ishaagrawal67 <isha.agrawal_cs20@gla.ac.in>
2022-06-24 11:49:18 +0530

    check your age

---
+++ src/checkyouareageis11ornot.java
@@ -0,0 +1,12 @@
+public class checkyouareageis11ornot {
+    public static void main(String[] args) {
+        int age=11;
+        if (age==11)
+        {
+            System.out.println("i am 11");
+        }
+        else {
+            System.out.println("i am not 11");
+        }
+    }
+}
