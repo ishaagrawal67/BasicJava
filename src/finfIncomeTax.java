dc921964b741471729fae57fc77e0e4f4b1ba816
ishaagrawal67 <isha.agrawal_cs20@gla.ac.in>
2022-06-24 11:53:28 +0530

    find income tax

---
+++ src/finfIncomeTax.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+public class finfIncomeTax {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        System.out.print("enter the income in lakh :");
+        float income=sc.nextFloat();
+        float tax=0;
+        if(income<=2.5f){
+            tax=tax=0;
+        }
+        else if(income>2.5f && income<=5.0f)
+        {
+            tax=tax+0.05f*(income-2.5f);
+        }
+        else if(income>5.0f && income<=10.0f)
+        {
+            tax=tax+0.05f*(5.0f-2.5f);
+            tax=tax+0.2f*(income-5.0f);
+        }
+        else if(income>10.0f)
+        {
+            tax=tax+0.05f*(5.0f-2.5f);
+            tax=tax=0.2f*(10.0f-5.0f);
+            tax=tax=0.3f*(income-10.0f);
+        }
+        System.out.println("The total tax paid is by the employee is : "+tax);
+    }
+
+}
