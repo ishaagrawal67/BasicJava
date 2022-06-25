2816b50caeacd424a37b527c5abf2b0fed9d7719
ishaagrawal67 <isha.agrawal_cs20@gla.ac.in>
2022-06-25 15:57:08 +0530

    print natural number from 100 to 200

---
+++ src/printNaturalFrom100to200.java
@@ -0,0 +1,11 @@
+public class printNaturalFrom100to200 {
+    public static void main(String[] args) {
+        int a=100;
+        while(a<=200)
+        {
+            System.out.println("naturl number from 100 to 200");
+            System.out.println(a);
+            a++;
+        }
+    }
+}
